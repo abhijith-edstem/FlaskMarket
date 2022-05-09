497a0a76feea600905c8ff3c1a44be6f6d776ba0
ABHIJITH U <abhijith.u@edstem.com>
2022-05-09 16:47:53 +0530

    flask market
    added user login before market page

---
+++ Market/routes.py
@@ -0,0 +1,58 @@
+from Market import app
+from flask import render_template, redirect, url_for, flash
+from Market.models import Item, User  #item package
+from Market.forms import RegisterForm, LoginForm
+from Market import db
+from flask_login import login_user, logout_user, login_required
+
+@app.route('/')       #decorator
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/market')
+@login_required
+def market_page():
+    items =Item.query.all()
+    return render_template('market.html',items=items)
+
+@app.route('/register', methods=['GET','POST'])
+def register_page():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                              email_address=form.email_address.data,
+                              password=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    if form.errors != {}:  #if there are no errors from the validations
+        for err_msg in form.errors.values():
+            flash(f'There was an error with creating a user:{err_msg}',category='danger')
+
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(
+                attempted_password=form.password.data
+        ):
+            login_user(attempted_user)
+            flash(f'Success! you are logged in as: {attempted_user.username}', category='success')
+            return redirect(url_for('market_page'))
+        else:
+            flash('Username and Password are not matching! please try again', category='danger')
+
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash("You have been logged out!",category='info')
+    return redirect(url_for("home_page"))
+
+
+
