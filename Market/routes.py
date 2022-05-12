a26989d097af8cfbfc713d8313e16e47b980f5a5
ABHIJITH U <abhijith.u@edstem.com>
2022-05-12 13:21:43 +0530

    completed the website creation
    completed flask

--- Market/routes.py
+++ Market/routes.py
@@ -1,7 +1,7 @@
 from Market import app
 from flask import render_template, redirect, url_for, flash, request
 from Market.models import Item, User  #item package
-from Market.forms import RegisterForm, LoginForm, PurchaseItemForm
+from Market.forms import RegisterForm, LoginForm, PurchaseItemForm, SellItemForm
 from Market import db
 from flask_login import login_user, logout_user, login_required, current_user
 
@@ -14,17 +14,34 @@ def home_page():
 @login_required
 def market_page():
     purchase_form = PurchaseItemForm()
+    selling_form = SellItemForm()
     if request.method == "POST":
+        #purchase item logic
         purchased_item = request.form.get('purchased_item')
         p_item_object = Item.query.filter_by(name=purchased_item).first()
         if p_item_object:
-            p_item_object.owner = current_user.id
-            current_user.budget -= p_item_object.price
-            db.session.commit()
-    items = Item.query.all()
+            if current_user.can_purchase(p_item_object):
+                p_item_object.buy(current_user)
+                flash(f"congratulation! you purchased {p_item_object.name} for {p_item_object.price}$", category='success')
+            else:
+                flash(f"unfortunately, you don't have enough money to purchase: {p_item_object.name}", category='danger')
+        #Sell Item Logic
+        sold_item = request.form.get('sold_item')
+        s_item_object = Item.query.filter_by(name=sold_item).first()
+        if s_item_object:
+            if current_user.can_sell(s_item_object):
+                s_item_object.sell(current_user)
+                flash(f"congratulation! you sold {s_item_object.name} back to market!", category='success')
+            else:
+                flash(f"something went wrong with selling {s_item_object.name}", category='danger')
 
 
-    return render_template('market.html',items=items, purchase_form=purchase_form)
+        return redirect(url_for('market_page'))
+
+    if request.method == "GET":
+        items = Item.query.filter_by(owner=None)
+        owned_items = Item.query.filter_by(owner=current_user.id)
+        return render_template('market.html', items=items, purchase_form=purchase_form, owned_items=owned_items, selling_form=selling_form)
 
 @app.route('/register', methods=['GET','POST'])
 def register_page():
@@ -63,7 +80,7 @@ def login_page():
 @app.route('/logout')
 def logout_page():
     logout_user()
-    flash("You have been logged out!",category='info')
+    flash("You have been logged out!", category='info')
     return redirect(url_for("home_page"))
 
 
